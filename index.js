5428bf0b872839b2ac18750bf7b093a56535584c
Steven <sbaudet@entreloups.com>
2019-06-17 16:03:29 +0200

    removeOrientationListener doesn't remove the listener

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ let screenHeight = Dimensions.get('window').height;
  * @return {number}              The calculated dp depending on current device's screen width.
  */
 const widthPercentageToDP = widthPercent => {
-  // Parse string percentage input and convert it to number.
-  const elemWidth = typeof widthPercent === "number" ? widthPercent : parseFloat(widthPercent);
+    // Parse string percentage input and convert it to number.
+    const elemWidth = typeof widthPercent === "number" ? widthPercent : parseFloat(widthPercent);
 
-  // Use PixelRatio.roundToNearestPixel method in order to round the layout
-  // size (dp) to the nearest one that correspons to an integer number of pixels.
-  return PixelRatio.roundToNearestPixel(screenWidth * elemWidth / 100);
+    // Use PixelRatio.roundToNearestPixel method in order to round the layout
+    // size (dp) to the nearest one that correspons to an integer number of pixels.
+    return PixelRatio.roundToNearestPixel(screenWidth * elemWidth / 100);
 };
 
 /**
@@ -29,16 +29,16 @@ const widthPercentageToDP = widthPercent => {
  * @return {number}               The calculated dp depending on current device's screen height.
  */
 const heightPercentageToDP = heightPercent => {
-  // Parse string percentage input and convert it to number.
-  const elemHeight = typeof heightPercent === "number" ? heightPercent : parseFloat(heightPercent);
+    // Parse string percentage input and convert it to number.
+    const elemHeight = typeof heightPercent === "number" ? heightPercent : parseFloat(heightPercent);
 
-  // Use PixelRatio.roundToNearestPixel method in order to round the layout
-  // size (dp) to the nearest one that correspons to an integer number of pixels.
-  return PixelRatio.roundToNearestPixel(screenHeight * elemHeight / 100);
+    // Use PixelRatio.roundToNearestPixel method in order to round the layout
+    // size (dp) to the nearest one that correspons to an integer number of pixels.
+    return PixelRatio.roundToNearestPixel(screenHeight * elemHeight / 100);
 };
 
 /**
- * Event listener function that detects orientation change (every time it occurs) and triggers 
+ * Event listener function that detects orientation change (every time it occurs) and triggers
  * screen rerendering. It does that, by changing the state of the screen where the function is
  * called. State changing occurs for a new state variable with the name 'orientation' that will
  * always hold the current value of the orientation after the 1st orientation change.
@@ -47,16 +47,21 @@ const heightPercentageToDP = heightPercent => {
  *                      invoke setState method and trigger screen rerender (this.setState()).
  */
 const listenOrientationChange = that => {
-  Dimensions.addEventListener('change', newDimensions => {
-    // Retrieve and save new dimensions
-    screenWidth = newDimensions.window.width;
-    screenHeight = newDimensions.window.height;
+    const orientationChangeHandler = newDimensions => {
+        // Retrieve and save new dimensions
+        screenWidth = newDimensions.window.width;
+        screenHeight = newDimensions.window.height;
 
-    // Trigger screen's rerender with a state update of the orientation variable
-    that.setState({
-      orientation: screenWidth < screenHeight ? 'portrait' : 'landscape'
-    });
-  });
+        // Trigger screen's rerender with a state update of the orientation variable
+        that.setState({
+            orientation: screenWidth < screenHeight ? 'portrait' : 'landscape'
+        });
+    }
+
+    Dimensions.addEventListener('change', orientationChangeHandler);
+
+    // Save this somewhere to path it back when removing the listener.
+    return orientationChangeHandler;
 };
 
 /**
@@ -65,13 +70,17 @@ const listenOrientationChange = that => {
  * listenOrientationChange function has been invoked. This should be done in order to
  * avoid adding new listeners every time the same component is re-mounted.
  */
-const removeOrientationListener = () => {
-  Dimensions.removeEventListener('change', () => {});
+const removeOrientationListener = orientationChangeHandler => {
+    // Remove the original handler if possible, otherwise path an empty function.
+    if (orientationChangeHandler) {
+        return Dimensions.removeEventListener('change', orientationChangeHandler);
+    }
+    Dimensions.removeEventListener('change', () => {});
 };
 
 export {
-  widthPercentageToDP,
-  heightPercentageToDP,
-  listenOrientationChange,
-  removeOrientationListener
+    widthPercentageToDP,
+    heightPercentageToDP,
+    listenOrientationChange,
+    removeOrientationListener
 };
